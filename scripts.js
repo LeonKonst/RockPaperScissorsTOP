6f909fa979b56251fe0f86d76e9ed69130903c03
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-09-01 19:00:21 +0200

    Create a function that dictates who wins the round

--- scripts.js
+++ scripts.js
@@ -7,3 +7,22 @@ function getHumanChoice(){
     let usersChoice = window.prompt("What's your choice? (choices: rock, paper, scissors)");   
     return usersChoice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice==="rock"&&computerChoice==="scissors"||
+        humanChoice==="paper"&&computerChoice==="rock"||
+        humanChoice==="scissors"&&computerChoice==="paper"){
+            humanScore +=1;
+            return `You won with ${humanChoice} against ${computerChoice}`
+        } else if(humanChoice===computerChoice) {
+            return `It's a tie! Both chose ${humanChoice}`
+        } else {
+            computerScore +=1;
+            return `You lost with ${humanChoice} against ${computerChoice}!`
+        }
+}
